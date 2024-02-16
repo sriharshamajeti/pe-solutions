9c5b85dbfdfc237f3e9f66e1fbaf0c11752ca0c3
sree <sriharshamajeti@gmail.com>
2024-02-16 12:43:26 -0600

    multiples of 3 or 5 solution, Project Euler

---
+++ multiples_of_3_or_5/multiples_of_3_or_5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class multiples_of_3_or_5 {
+
+    public static int sum_of_number_multiples(int limit, int num) {
+        int n;
+        n = (limit-1)/num; //no.of multiples divisible by num within the limit
+
+        return (num) * (n*(n + 1)/2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int limit = sc.nextInt();
+
+        System.out.println(sum_of_number_multiples(limit, 3)
+                + sum_of_number_multiples(limit, 5)
+                - sum_of_number_multiples(limit, 15));
+
+        sc.close();
+    }
+}
